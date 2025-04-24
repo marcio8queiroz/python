827684a67e6258b0fced0bf5c84978b401eb42b8
marcio8queiroz <marcio8queiroz@hotmail.com>
2025-04-23 21:28:16 -0400

    update

--- arquivos_02.py
+++
@@ -1,9 +0,0 @@
-
-with open("script-dados-modulo-agenda.txt", "r") as arquivo:
-    conteudo_atual = arquivo.read()
-    print(conteudo_atual)
-
-with open("script-dados-modulo-agenda.txt", "a") as arquivo:
-    arquivo.write("adicionado no final do arquivo")
-  
-    
